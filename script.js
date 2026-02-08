f2ef69c3076bb314ba0b64ec6037d90b7b5ac640
David Zhang <davidzhang21@berkeley.edu>
2026-02-07 17:16:18 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const noBtn = document.getElementById("noBtn");
+if (noBtn) {
+    noBtn.addEventListener("click", () => {
+        window.location.href = "no.html";
+    });
+}
+
+const returnBtn = document.getElementById("returnBtn");
+if (returnBtn) {
+    returnBtn.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+}
+
+const yesBtn = document.getElementById("yesBtn");
+if (yesBtn) {
+    yesBtn.addEventListener("click", () => {
+        window.location.href = "yes.html";
+    });
+}
+
+const yesValBtn = document.getElementById("yesValBtn");
+if (yesValBtn) {
+    yesValBtn.addEventListener("click", () => {
+        window.location.href = "yesval.html";
+    });
+}
+
+const yesValBtn2 = document.getElementById("yesValBtn2");
+if (yesValBtn2) {
+    yesValBtn2.addEventListener("click", () => {
+        window.location.href = "yesval.html";
+    });
+}
+
+const noValBtn = document.getElementById("noValBtn");
+if (noValBtn) {
+    noValBtn.addEventListener("click", () => {
+        window.location.href = "noval.html";
+    });
+}
+
+const sorry = document.getElementById("sorry");
+if (sorry) {
+    sorry.addEventListener("click", () => {
+        window.location.href = "valentine_no_no.html";
+    });
+}
